dd41763c94ca2c87f95fb72d72707676b70d3e61
Emil-Pestean <emil@knoxon.co.uk>
2020-06-06 03:36:24 +0300

    Hello world with some additions

---
+++ 03-hello_world/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello_world")
+
+	// call another function
+	foo()
+
+	fmt.Println("Another line")
+
+}
+
+func foo() {
+
+	fmt.Println("I'm in foo, hu3hu3")
+
+}
+
+// Control flow
+// 1 - sequence
+// 2 - loop / iterative
+// 3 - conditional
