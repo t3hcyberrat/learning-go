65658fc26c4b8f925275becc36ae3226efc8e972
Emil-Pestean <emil.pestean@gmail.com>
2020-06-19 13:38:15 +0300

    Added string transform

--- 08-fmt-package/main.go
+++ 08-fmt-package/main.go
@@ -7,14 +7,20 @@ var y = 911
 func main() {
 
 	fmt.Println(y)
-	fmt.Printf("%T\n", y)
-	fmt.Printf("%b\n", y)
-	fmt.Printf("%x\n", y)
-	fmt.Printf("%#X\n", y)
+	fmt.Printf("%T\n", y)  // print type
+	fmt.Printf("%b\n", y)  // print binary
+	fmt.Printf("%x\n", y)  // print hex without prededing 0x
+	fmt.Printf("%#X\n", y) // print hex with preceding 0x
 
 	y = 100
 
 	fmt.Printf("%#X", y)
-	fmt.Printf("%#X\t%b\t%x\n", y, y, y)
+	fmt.Printf("%#X\t%b\t%x\n", y, y, y) // print using tab between values
+
+	s := fmt.Sprintf("%#X\t%b\t%x\n", y, y, y) // transform to string
+	fmt.Println(s)                             // print as str
+	fmt.Printf("%T\n", s)                      // check if type is str
+
+	fmt.Printf("%v\n", y) // back to normal value
 
 }
